90d2cf898479fdea265073a166fd809bc519f127
kececirecep <kececirecep@outlook.com>
2024-05-21 17:40:20 +0300

    responsive mobile menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import Logo from './Logo'
-import Menu from './Menu'
-import Search from './Search'
+import Menu from './Menu' 
 
 const Header = () => {
     return (
         <div className='bg-transparent py-4 absolute w-full'>
             <div className="container flex items-center justify-between">
                 <Logo />
-                <Menu />
-                <Search />
+                <Menu /> 
             </div>
         </div>
     )
--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -1,17 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
+import { LuShoppingCart } from "react-icons/lu";
+import { IoMdHeartEmpty } from "react-icons/io";
 
 const Menu = () => {
+  const [mobileMenuToggle, setMobileMenuToggle] = useState(false);
+
+  const menuContent = [
+    {
+      text: "Home",
+      url: "/",
+    }, {
+      text: "About",
+      url: "/",
+    }, {
+      text: "Pricing",
+      url: "/",
+    }, {
+      text: "Blog",
+      url: "/",
+    },
+  ]
+
+  const mobileToggleClick = () => {
+    setMobileMenuToggle(!mobileMenuToggle)
+  }
+
   return (
-    <nav>
+    <div>
       <ul className='flex items-center gap-12 font-semibold text-14'>
-        <li>Retail</li>
-        <li>SME</li>
-        <li>Corporate</li>
-        <li>About Us </li>
-        <li>Covid 19</li>
-        <li>Campgains</li>
-      </ul> 
-    </nav>
+        <div className={`fixed top-0 ${mobileMenuToggle ? "left-0" : "-left-[75%]"}  bg-[#000000ce] text-white flex flex-col justify-center items-center text-4xl font-semibold h-full w-[75%] z-50 md:hidden duration-700`}>
+          <ul>
+            {
+              menuContent.map((item, index) => {
+                return (
+                  <NavLink to="" key={index}><li className='flex items-center py-4 hover:text-my-pink duration-200'>{item.text}</li></NavLink>
+                )
+              })
+            } 
+          </ul>
+        </div>
+ 
+      </ul>
+
+      <nav className='hidden md:flex'>
+        <ul className='flex items-center gap-9 font-[400] text-lg'>
+          {
+            menuContent.map((item, index) => {
+              return (
+                <NavLink to="" key={index}><li className='flex items-center'>{item.text}</li></NavLink>
+              )
+            })
+          }
+          
+        </ul>
+      </nav>
+
+      <div className='md:hidden flex fixed top-7 right-6 cursor-pointer z-50'>
+        <span className='text-black' onClick={mobileToggleClick}>{mobileMenuToggle ? <IoClose size={"26px"} /> : <RxHamburgerMenu size={"26px"} />}</span>
+      </div>
+    </div>
   )
 }
 
